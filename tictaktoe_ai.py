41f07ff29edf85bea1f3cbaaba7bfb9424be4579
Mithilesh Kapadi <mithileshkapadi08@gmail.com>
2021-06-07 15:54:59 +0530

    ai added

--- tictaktoe_ai.py
+++ tictaktoe_ai.py
@@ -47,6 +47,25 @@ def checkForWin():
     else:
         return False
 
+def checkWhichMarkWon(mark):
+    if board[1] == board[2] and board[1] == board[3] and board[1] != mark:
+        return True
+    elif board[4] == board[5] and board[4] == board[6] and board[4] != mark:
+        return True
+    elif board[7] == board[8] and board[7] == board[9] and board[7] != mark:
+        return True
+    elif board[1] == board[4] and board[1] == board[7] and board[1] != mark:
+        return True
+    elif board[2] == board[5] and board[2] == board[8] and board[2] != mark:
+        return True
+    elif board[3] == board[6] and board[3] == board[9] and board[3] != mark:
+        return True
+    elif board[1] == board[5] and board[1] == board[9] and board[1] != mark:
+        return True
+    elif board[7] == board[5] and board[7] == board[3] and board[7] != mark:
+        return True
+    else:
+        return False
 
 def insertLetter(letter, position):
     if spaceIsFree(position):
@@ -78,6 +97,51 @@ def playerMove():
     return
 
 def compMove():
-    position = int(input("Enter the position for 'X' : "))
-    insertLetter(bot,position)
+    #position = int(input("Enter the position for 'X' : "))
+    bestScore = -1000
+    bestMove = 0
+    for key in board.keys():
+        if board[key] == " ":
+            board[key] = bot
+            score = minimax(board,0,False)
+            board[key] = " "
+            if score > bestScore:
+                bestScore = score
+                bestMove = key
+    insertLetter(bot,bestMove)
     return
+
+def minimax(board, depth, isMaximizing):
+    if checkWhichMarkWon(bot):
+        return 1
+    elif checkWhichMarkWon(player):
+        return -1
+    elif checkDraw():
+        return 0
+    
+    if isMaximizing:
+        bestScore = -1000
+        for key in board.keys():
+            if board[key] == " ":
+                board[key] = bot
+                score = minimax(board, 0, False)
+                board[key] = " "
+                if score > bestScore:
+                    bestScore = score
+                    bestMove = key
+        return bestScore
+    
+    else:
+        bestScore = 8000
+        for key in board.keys():
+            if board[key] == " ":
+                board[key] = bot
+                score = minimax(board, depth+1, True)
+                board[key] = " "
+                if score < bestScore:
+                    bestScore = score
+                    bestMove = key 
+
+while not checkForWin():
+    playerMove()
+    compMove()
